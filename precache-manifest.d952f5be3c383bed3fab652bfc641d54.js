e8c07a0e28e584bc659d22a3bc8eb151b887f2db
harsh3105 <harshgrover3105@gmail.com>
2020-05-09 19:22:53 +0530

    Updates

---
+++ precache-manifest.d952f5be3c383bed3fab652bfc641d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f0510541a914b3caabd713651d9af8",
+    "url": "/react-covid19-application/index.html"
+  },
+  {
+    "revision": "b41ab6714678ed9194fe",
+    "url": "/react-covid19-application/static/css/main.ad78f289.chunk.css"
+  },
+  {
+    "revision": "55dfb4b73f583d2d28b7",
+    "url": "/react-covid19-application/static/js/2.67e39494.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid19-application/static/js/2.67e39494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41ab6714678ed9194fe",
+    "url": "/react-covid19-application/static/js/main.8d854d6f.chunk.js"
+  },
+  {
+    "revision": "117de5c326fcb7eb4ab1",
+    "url": "/react-covid19-application/static/js/runtime-main.58d1e9ac.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid19-application/static/media/images.d7265326.png"
+  }
+]);
